c2963907cb52d12fd240fbec3465f3b4d4689c0c
TrueFe3r <93656095+TrueFe3r@users.noreply.github.com>
2022-03-16 22:16:43 +0200

    Update ben.py

--- ben.py
+++ ben.py
@@ -1,11 +1,10 @@
-""" Бен на МАКСИМАЛКАХ!))
-Команда: .ben2 автор @ToxicUse"""
+
 from telethon import events
 import requests
 from uniborg.util import admin_cmd
 import random as rd
 
-@borg.on(admin_cmd("ben2"))
+@borg.on(admin_cmd("ben"))
 async def _(event):
     if event.fwd_from:
         return
@@ -27,4 +26,38 @@ async def _(event):
         reply_to=message_id
     )
     await event.delete()
-    #Зачем ты это читал 
+    #Бэн, когда?
+@borg.on(admin_cmd("benwhen"))
+async def _(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    num = rd.randint(1, 10)
+    if num == 1:
+        m = "Бен говорит: Через 10 минут."
+    if num == 2:
+        m = "Бэн говорит: Через неделю."
+    if num == 3:
+    	m = "Бэн говорит: Завтра."
+    if num == 4:
+    	m = "Бэн говорит: Никогда..."
+    if num == 5:
+    	m = "Бэн говорит: Не уверен, спроси позже."
+    if num == 6:
+    	m = "Бэн говорит: Скоро."
+    if num == 7:
+    	m = "Бэн говорит: Послезавтра."
+    if num == 8:
+    	m = "Бэн говорит: Через месяц-полтора."
+    if num == 9:
+    	m = "Бэн говорит: Вчера."
+    if num == 10:
+    	m = "Бэн говорит: Когда тебе стукнет 72."
+    await borg.send_message(
+        event.chat_id,
+        m,
+        reply_to=message_id
+    )
+    await event.delete()
